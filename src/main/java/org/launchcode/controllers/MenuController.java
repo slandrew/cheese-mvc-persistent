ea068cdf70187d1aab44ca1bacbaa8d85560bfbe
Andrew Dixon <infinite_number@yahoo.com>
2019-06-19 14:31:55 -0500

    beginning work on menu view

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,29 @@
+package org.launchcode.controllers;
+
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "My Menus");
+
+        return "menu/index";
+    }
+}
